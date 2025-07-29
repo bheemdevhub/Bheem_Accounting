23facfc485a145efc18686abcb23879084e145dd
bheemdevhub <devhub@bheemverse.com>
2025-07-29 16:09:29 +0530

    chore: add and update setup.py for editable install

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='bheem-accounting',
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[], # Add module-specific dependencies
+    packages=find_packages(include=["accounting", "accounting.*"]),
+    install_requires=[],
     include_package_data=True,
     description='Bheem Accounting ERP module',
     author='Bheem Core Team',
